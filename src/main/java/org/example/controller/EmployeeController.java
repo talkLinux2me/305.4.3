d4de98c32aa1afb987d44c5ce3f37d98fddf3717
talkLinux2me <167791277+talkLinux2me@users.noreply.github.com>
2024-08-23 09:22:30 -0400

    first commit

---
+++ src/main/java/org/example/controller/EmployeeController.java
@@ -0,0 +1,17 @@
+package org.example.controller;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+public class EmployeeController {
+
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration().configure().buildSessionFactory();
+        Session session = factory.openSession();
+
+
+        factory.close();
+        session.close();
+    }
+
+
+}
